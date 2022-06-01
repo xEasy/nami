4cdcb96b80c084b222a539b00a89b5578ad59aac
xEasy <yijo1001@gmail.com>
2022-06-01 14:30:31 +0800

    [feature] service regiest

---
+++ service.go
@@ -0,0 +1,74 @@
+package nami
+
+import (
+	"fmt"
+	"go/ast"
+	"reflect"
+	"sync/atomic"
+)
+
+type service struct {
+	name   string
+	typ    reflect.Type           // struct's type
+	rcvr   reflect.Value          // struct obj itself
+	method map[string]*methodType // struct's method
+}
+
+func newService(rcvr any) *service {
+	s := new(service)
+	s.rcvr = reflect.ValueOf(rcvr)
+	s.name = reflect.Indirect(s.rcvr).Type().Name()
+	s.typ = reflect.TypeOf(rcvr)
+	if !ast.IsExported(s.name) {
+		fmt.Printf("rpc service: %s is not a valid service name", s.name)
+	}
+	s.registMethods()
+	return s
+}
+
+func (s *service) registMethods() {
+	s.method = make(map[string]*methodType)
+
+	for i := 0; i < s.typ.NumMethod(); i++ {
+		method := s.typ.Method(i)
+		mType := method.Type
+
+		// skip if argv's count not mathing
+		if mType.NumIn() != 3 || mType.NumOut() != 1 {
+			continue
+		}
+
+		// skip if method not return error
+		fmt.Println(mType.Out(0).Kind().String())
+
+		if mType.Out(0) != reflect.TypeOf((*error)(nil)).Elem() {
+			continue
+		}
+		argType, replyType := mType.In(1), mType.In(2)
+		if !isExportedOrBuiltinType(argType) || !isExportedOrBuiltinType(replyType) {
+			continue
+		}
+
+		s.method[method.Name] = &methodType{
+			method:    method,
+			ArgType:   argType,
+			ReplyType: replyType,
+		}
+
+		fmt.Printf("rpc server: regist %s.%s \n", s.name, method.Name)
+	}
+}
+
+func (s *service) call(m *methodType, argv, replyv reflect.Value) error {
+	atomic.AddUint64(&m.numCalls, 1)
+	f := m.method.Func
+	returnValues := f.Call([]reflect.Value{s.rcvr, argv, replyv})
+	if errInter := returnValues[0].Interface(); errInter != nil {
+		return errInter.(error)
+	}
+	return nil
+}
+
+func isExportedOrBuiltinType(t reflect.Type) bool {
+	return ast.IsExported(t.Name()) || t.PkgPath() == ""
+}
