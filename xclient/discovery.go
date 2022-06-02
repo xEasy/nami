bc1fe2cd4065405f1d2a7a7cf20d7a6507f39028
xEasy <yijo1001@gmail.com>
2022-06-02 12:55:02 +0800

    [feature] load balance xclient

---
+++ xclient/discovery.go
@@ -0,0 +1,84 @@
+package xclient
+
+import (
+	"errors"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type SelectMode int
+
+const (
+	RandomSelect     SelectMode = iota // select randomly
+	RoundRobinSelect                   // select using Robbin algorithm
+)
+
+type Discovery interface {
+	Refresh() error
+	Update(servers []string) error
+	Get(mode SelectMode) (string, error)
+	GetAll() ([]string, error)
+}
+
+// MultiServersDiscovery is a Discovery for mutil servers without a regiestry center (like etcd, consoul...)
+type MultiServersDiscovery struct {
+	r       *rand.Rand // generate random number
+	mu      sync.Mutex // protect following
+	servers []string
+	index   int // record the selected position for robin algorithm
+}
+
+var _ Discovery = (*MultiServersDiscovery)(nil)
+
+// Refresh doesn't make sense for MultiServersDiscovery
+func (m *MultiServersDiscovery) Refresh() error {
+	return nil
+}
+
+func (m *MultiServersDiscovery) Update(servers []string) error {
+	m.mu.Lock()
+	m.servers = servers
+	m.mu.Unlock()
+	return nil
+}
+
+// Get a server according to mode
+func (m *MultiServersDiscovery) Get(mode SelectMode) (string, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	n := len(m.servers)
+	if n == 0 {
+		return "", errors.New("rpc discovery: no available servers")
+	}
+
+	switch mode {
+	case RandomSelect:
+		return m.servers[m.r.Intn(n)], nil
+	case RoundRobinSelect:
+		s := m.servers[m.index%n]
+		m.index = (m.index + 1) % n
+		return s, nil
+	default:
+		return "", errors.New("rpc discovery: not supported select mode")
+	}
+}
+
+// return a copy of all servers
+func (m *MultiServersDiscovery) GetAll() ([]string, error) {
+	m.mu.Lock()
+	servers := make([]string, len(m.servers), len(m.servers))
+	copy(servers, m.servers)
+	m.mu.Unlock()
+	return servers, nil
+}
+
+func NewMultiServersDiscovery(servers []string) Discovery {
+	d := &MultiServersDiscovery{
+		servers: servers,
+		r:       rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+	d.index = d.r.Intn(math.MaxInt32 - 1)
+	return d
+}
---
+++ xclient/xclient.go
@@ -0,0 +1,114 @@
+package xclient
+
+import (
+	"context"
+	"io"
+	"reflect"
+	"sync"
+
+	"github.com/xeasy/nami"
+	"github.com/xeasy/nami/client"
+)
+
+type XClient struct {
+	d       Discovery
+	mode    SelectMode
+	opt     *nami.Option
+	mu      sync.Mutex
+	clients map[string]client.NClient // client cache
+}
+
+// make sure XClient represented io.Closer interface
+var _ io.Closer = (*XClient)(nil)
+
+func NewXClient(d Discovery, mode SelectMode, opt *nami.Option) *XClient {
+	return &XClient{
+		d:    d,
+		mode: mode,
+		opt:  opt,
+	}
+}
+
+func (x *XClient) Close() error {
+	x.mu.Lock()
+	defer x.mu.Unlock()
+
+	for key, client := range x.clients {
+		client.Close()
+		delete(x.clients, key)
+	}
+	return nil
+}
+
+// dial get a cache client, make a new client if cached client unavailable
+func (xc *XClient) dial(rpcAddr string) (client.NClient, error) {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+
+	cli, ok := xc.clients[rpcAddr]
+	if ok && !cli.IsAvailable() {
+		cli.Close()
+		delete(xc.clients, rpcAddr)
+		cli = nil
+	}
+
+	if cli == nil {
+		var err error
+		cli, err = client.XDial(rpcAddr, xc.opt)
+		if err != nil {
+			return nil, err
+		}
+		xc.clients[rpcAddr] = cli
+	}
+	return cli, nil
+}
+
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply any) error {
+	cli, err := xc.dial(rpcAddr)
+	if err != nil {
+		return err
+	}
+	return cli.Call(ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply any) error {
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Broadcast(ctx context.Context, serviceMethod string, args, reply any) error {
+	servers, err := xc.d.GetAll()
+	if err != nil {
+		return err
+	}
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	var e error
+	replyDone := reply == nil // if reply is nil, don't need to set value
+	ctx, cancel := context.WithCancel(ctx)
+	for _, rpcAddr := range servers {
+		wg.Add(1)
+		go func(rpcAddr string) {
+			defer wg.Done()
+			var cloneReply any
+			if reply != nil {
+				cloneReply = reflect.New(reflect.ValueOf(reply).Elem().Type()).Interface()
+			}
+			err := xc.call(rpcAddr, ctx, serviceMethod, args, cloneReply)
+			if err != nil && e != nil {
+				e = err
+				cancel()
+			}
+			if err == nil && !replyDone {
+				reflect.ValueOf(reply).Elem().Set(reflect.ValueOf(cloneReply).Elem())
+				replyDone = true
+			}
+			mu.Unlock()
+		}(rpcAddr)
+	}
+	wg.Wait()
+	return e
+}
